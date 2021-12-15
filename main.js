1a70b6ab0e6693eabf614901caeb66eaf5742310
Arthur Melton <amtitan@icloud.com>
2021-12-15 07:50:47 -0500

    got most things but I want the buffer to be an vec

---
+++ main.js
@@ -0,0 +1,54 @@
+function update_dark_mode() {
+    if (localStorage.getItem("DarkMode") !== null) {
+        if (localStorage.getItem("DarkMode") == "true") {
+            document.documentElement.classList.add("dark");
+        }
+        else {
+            document.documentElement.classList.remove("dark");
+        }
+    }
+    else {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            localStorage.setItem("DarkMode", "true");
+            document.documentElement.classList.add("dark");
+        }
+        else {
+            localStorage.setItem("DarkMode", "false");
+            document.documentElement.classList.remove("dark");
+        }
+    }
+}
+
+update_dark_mode();
+
+function init() {
+    document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+}
+  
+function handleFileSelect(event) {
+    const reader = new FileReader()
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+}
+
+function handleFileLoad(event) {
+    document.getElementById('fileInput').style = "display:none";
+    var sections = event.target.result.match(/[\s\S]{1,45000}/g)
+    upload(sections, 0);
+}
+
+function upload(sections, int) {
+    if (int<sections.length) {
+        const promise1 = new Promise((resolve) => {
+            $.post("upload", sections[int], function(status) {
+                resolve();
+            })
+        })
+        promise1.then(() => {
+            upload(sections, int+=1);
+        })
+    }
+    else {
+        $.post("upload", "done");
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
